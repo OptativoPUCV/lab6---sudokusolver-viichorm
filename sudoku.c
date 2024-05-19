39b15ecf633a7b349b747a9745d60a306183f072
Your Name <you@example.com>
2024-05-19 01:07:32 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -131,8 +131,19 @@ List* get_adj_nodes(Node* n){
     return list;
 }
 
+// ejercicio 4
 
 int is_final(Node* n){
+
+   for (int i = 0; i < 9; i++){
+
+      for (int j = 0; j < 9; j++){
+
+         if (n->sudo[i][j] == 0){
+            return 0;
+         }
+      }
+   }
     return 0;
 }
 
